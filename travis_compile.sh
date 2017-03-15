78015f1935b79370b48ff1cc0500f577ca9a9f3d
zoltan.fodor <zoltan.fodor@pitechnologies.ro>
2017-03-15 16:36:48 +0200

    Explicitly set up the package branch for travis compose.

--- travis_compile.sh
+++ travis_compile.sh
@@ -17,6 +17,7 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 # Checkout target branch and create if it doesn't exists.
 echo "Checking out $TARGET_BRANCH."
+git remote set-branches --add origin $TARGET_BRANCH
 git fetch origin
 git checkout -b $TARGET_BRANCH origin/$TARGET_BRANCH  || git checkout -b $TARGET_BRANCH --track origin/$SOURCE_BRANCH && TARGET_NEW=1 || exit 0
 
